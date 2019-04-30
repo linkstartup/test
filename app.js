324f130d3bdca525ef3e18fd8b6f12972f6d17d0
david <apple@AppledeMacBook-Pro-3.local>
2019-04-30 17:56:02 +0900

    server

--- app.js
+++ app.js
@@ -1242,7 +1242,7 @@ db.connect((err) => {
     // Start up our Express Application
     // And listen for Request
     else {
-        app.listen(80, () => {
+        app.listen(8365, () => {
 
             console.log('connected to database, app listening on port 80');
         });
